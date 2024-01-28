0d69c24c04bcab99c3411ba31778cec13a820046
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-01-29 00:02:44 +0530

    Refactor authLayout function and add navigation links

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,7 +1,31 @@
-export default function authLayout({ children }) {
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function AuthLayout({ children }) {
+  const navLinks = [
+    { name: "Login", href: "/login" },
+    { name: "Register", href: "/register" },
+    { name: "Forgot Password", href: "/forgot-password" },
+  ];
+
+  const pathName = usePathname();
   return (
     <div>
       <h1>Auth Layout</h1>
+      {navLinks.map((link) => {
+        const activePath = pathName.startsWith(link.href);
+        const activeStyle = activePath ? "bg-blue-500" : "";
+        return (
+          <Link
+            href={link.href}
+            key={link.name}
+            className={`mx-3 border-white border-2 px-3 ${activeStyle}`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
       <div>{children}</div>
     </div>
   );
