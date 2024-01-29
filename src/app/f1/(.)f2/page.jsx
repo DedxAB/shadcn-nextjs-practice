19d77fd4c0171d3617d0342bffe6b3583ce335ab
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-01-29 20:39:11 +0530

    Add F1 and F2 pages with routing

---
+++ src/app/f1/(.)f2/page.jsx
@@ -0,0 +1,10 @@
+const InterceptedF2 = () => {
+  return (
+    <>
+      (.) Intercepted F2 page
+      <h1>After reloading u will get the actual F2 page</h1>
+    </>
+  );
+};
+
+export default InterceptedF2;
