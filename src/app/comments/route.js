2404eace62934014cb1f2811ce4f1ade0d989df0
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-01-31 02:27:20 +0530

    Add comments data and route files

---
+++ src/app/comments/route.js
@@ -0,0 +1,38 @@
+import { comments } from "./data";
+
+export async function GET() {
+  return Response.json(comments);
+}
+
+// ## we cannot do this in Next JS
+// export async function GET() {
+//   return {
+//     body: JSON.stringify(comments),
+//     headers: { "Content-Type": "application/json" },
+//     status: 200,
+//   };
+// }
+
+// ## We cannot do this in Next JS
+// export async function GET(req, res) {
+//   return res.status(200).json(comments);
+// }
+
+export async function POST(request) {
+  const { text } = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text,
+  };
+  comments.push(newComment);
+  // return Response.json(newComment);
+
+  // or do below
+
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "content-type": "application/json",
+    },
+    status: 201,
+  });
+}
