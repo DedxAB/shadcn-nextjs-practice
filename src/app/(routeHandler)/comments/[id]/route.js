ce07e067ee9f4baff9e416790459adbf0f1bdd15
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-01-31 17:35:55 +0530

    Add PATCH route to update comment text

--- src/app/(routeHandler)/comments/[id]/route.js
+++ src/app/(routeHandler)/comments/[id]/route.js
@@ -5,3 +5,12 @@ export async function GET(_req, { params }) {
   const comment = comments.find((comment) => comment.id === parseInt(id));
   return Response.json(comment);
 }
+
+export async function PATCH(req, { params }) {
+  // This is one way to do the innter text to update
+  const { id } = params;
+  const comment = comments.find((comment) => comment.id === parseInt(id));
+  const { text } = await req.json();
+  comment.text = text;
+  return Response.json(comment);
+}
