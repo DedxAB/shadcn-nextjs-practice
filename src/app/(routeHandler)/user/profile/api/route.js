24b8d4706c23efde871211a9725b7d68b6658cca
Arnab Bhoumik <arnab.officialcorp@gmail.com>
2024-02-01 23:37:41 +0530

    Refactor user profile API route to include authorization token check

--- src/app/(routeHandler)/user/profile/api/route.js
+++ src/app/(routeHandler)/user/profile/api/route.js
@@ -1,7 +1,10 @@
-export async function GET() {
-  return new Response(`User profile data`);
-}
-
-export async function POST() {
-  return new Response(`User profile data`);
+export async function GET(request) {
+  const requestHeader = new Headers(request.headers);
+  const token = requestHeader.get("Authorization");
+  console.log(token);
+  return new Response(`<h1>Profile Api data</h1>`, {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
